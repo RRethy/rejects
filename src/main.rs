4e9bfb3b1aceb1a7efd737c82c8e3887ba2cfa48
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-05-06 16:22:59 -0400

    Try out a more complicated pattern

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod parser;
 
 fn main() {
-    let res = parser::Parser::parse("(abc|abd|a)*");
+    let res = parser::Parser::parse("(a|b)*(http:|https:)//www google com+");
     if let Ok(regex) = res {
         println!("{:?}", regex);
-        println!("{}", regex.find("ab"));
+        println!("{}", regex.find("http://www google com"));
+        println!("{}", regex.find("abababaaahttp://www google com"));
+        println!("{}", regex.find("https://www google commmmm"));
+        println!("{}", regex.find("https://www google com"));
+        println!("{}", regex.find("http://www google com"));
+        println!("{}", regex.find("http://www google co"));
         println!("{}", regex.find("dabc"));
         println!("{}", regex.find("abc"));
         println!("{}", regex.find("abd"));
